4dad1e9eb189c756d34a274f00997534ea09d0da
AdkMarius <adkmarius01@gmail.com>
2024-11-25 11:10:48 +0100

    change HumanDashGame class

--- HumanDashGame.cs
+++ HumanDashGame.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,8 +7,28 @@ namespace HumanDash;
 
 public class HumanDashGame : Game
 {
+    private const string RUNNER_TEXTURE_NAME = "runner";
+    private const string PNEU_TEXTURE_NAME = "pneu";
+    private const string OISEAUX_TEXTURE_NAME = "oisaux";
+    private const string PILE_DE_CARTONS_TEXTURE_NAME = "pile_de_cartons";
+    private const string DECOR_TEXTURE_NAME = "decor";
+    private const string BRIQUES_TEXTURE_NAME = "briques";
+    private const string BUTTON_PRESS_SOUND_NAME = "button-press";
+    private const string HIT_OBSTACLE_SOUND_NAME = "hit";
+    private const string SCORE_REACHED_SOUND_NAME = "score";
+    
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    
+    private Texture2D _runnerTexture;
+    private Texture2D _pneuTexture;
+    private Texture2D _oisauxTexture;
+    private Texture2D _pile_de_cartonsTexture;
+    private Texture2D _decorTexture;
+    private Texture2D _briquesTexture;
+    private SoundEffect _buttonPressSound;
+    private SoundEffect _scoreReachedSound;
+    private SoundEffect _hitObstacleSound;
 
     public HumanDashGame()
     {
@@ -28,6 +49,7 @@ public class HumanDashGame : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        _runnerTexture = Content.Load<Texture2D>(RUNNER_TEXTURE_NAME);
     }
 
     protected override void Update(GameTime gameTime)
